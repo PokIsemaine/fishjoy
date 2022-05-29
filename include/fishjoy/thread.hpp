ea67b60377cbb205f3969f019a8a6472329b8062
fishingrod <851892190@qq.com>
2022-05-29 23:42:08 +0800

    update Semaphore & Mutex

--- include/fishjoy/thread.hpp
+++ include/fishjoy/thread.hpp
@@ -5,41 +5,188 @@
 #ifndef FISHJOY_THREAD_HPP
 #define FISHJOY_THREAD_HPP
 
-#include <thread>
+#include <pthread.h>
+
 #include <functional>
 #include <memory>
-#include <pthread.h>
 #include <string>
+#include <thread>
+#include <semaphore.h>
 
-namespace fishjoy {
-  class Thread {
+namespace fishjoy
+{
+  class Semaphore {
    public:
-    typedef std::shared_ptr<Thread> ptr;
-    Thread(std::function<void()> cb, const std::string& name);
+    Semaphore(uint32_t count = 0);
+    ~Semaphore();
+
+    void wait();
+    void notify();
+
+    Semaphore(const Semaphore&) = delete;
+    Semaphore(const Semaphore&&) = delete;
+    Semaphore& operator=(const Semaphore&) = delete;
+   private:
+    sem_t m_semaphore;
+  };
+
+  template<typename T>
+  struct ScopedLockImpl {
+   public:
+      ScopedLockImpl(T& mutex)
+        :m_mutex(mutex) {
+        mutex.lock();
+        m_locked = true;
+      }
+
+      ~ScopedLockImpl() {
+        unlock();
+      }
+
+      void lock(){
+        if(!m_locked) {
+          m_mutex.lock();
+          m_locked = true;
+        }
+      }
+
+      void unlock() {
+        if(m_locked) {
+          m_mutex.unlock();
+          m_locked = false;
+        }
+      }
+   private:
+    T& m_mutex;
+    bool m_locked;
+  };
+
+  template<typename T>
+  struct ReadScopedLockImpl {
+   public:
+    ReadScopedLockImpl(T& mutex)
+        :m_mutex(mutex) {
+      mutex.lock();
+      m_locked = true;
+    }
+
+    ~ReadScopedLockImpl() {
+      unlock();
+    }
+
+    void lock(){
+      if(!m_locked) {
+        m_mutex.rdlock();
+        m_locked = true;
+      }
+    }
+
+    void unlock() {
+      if(m_locked) {
+        m_mutex.unlock();
+        m_locked = false;
+      }
+    }
+   private:
+    T& m_mutex;
+    bool m_locked;
+  };
+
+  template<typename T>
+  struct WriteScopedLockImpl {
+   public:
+    WriteScopedLockImpl(T& mutex)
+        :m_mutex(mutex) {
+      mutex.lock();
+      m_locked = true;
+    }
+
+    ~WriteScopedLockImpl() {
+      unlock();
+    }
+
+    void lock(){
+      if(!m_locked) {
+        m_mutex.lock();
+        m_locked = true;
+      }
+    }
+
+    void unlock() {
+      if(m_locked) {
+        m_mutex.unlock();
+        m_locked = false;
+      }
+    }
+   private:
+    T& m_mutex;
+    bool m_locked;
+  };
+
+  class RWMutex {
+   public:
+    using ReadLock = ReadScopedLockImpl<RWMutex>;
+    using WriteLock = WriteScopedLockImpl<RWMutex>;
+
+    RWMutex() {
+      pthread_rwlock_init(&m_lock, nullptr);
+    }
+
+    ~RWMutex() {
+      pthread_rwlock_destroy(&m_lock);
+    }
+
+    void rdlock() {
+      pthread_rwlock_rdlock(&m_lock);
+    }
+
+    void wrlock() {
+      pthread_rwlock_wrlock(&m_lock);
+    }
+
+    void unlock() {
+      pthread_rwlock_unlock(&m_lock);
+    }
+   private:
+    pthread_rwlock_t m_lock;
+  };
+  class Thread
+  {
+   public:
+    using ptr = std::shared_ptr<Thread>;
+    Thread(std::function<void()> callback, const std::string& name);
     ~Thread();
 
-    pid_t getId() const { return m_id;}
-    const std::string& getName() const { return m_name;}
+    pid_t getId() const
+    {
+      return m_id;
+    }
+    const std::string& getName() const
+    {
+      return m_name;
+    }
 
     void join();
 
     static Thread* GetThis();
     static const std::string& GetName();
     static void SetName(const std::string& name);
-   private:
+
     //互斥量、互斥信号不能拷贝构造
     Thread(const Thread&) = delete;
     Thread(const Thread&&) = delete;
     Thread& operator=(const Thread&) = delete;
 
-    static void* run(void* arg);
    private:
+    static void* run(void* arg);
+
     pid_t m_id = -1;
     pthread_t m_thread = 0;
-    std::function<void()> m_cb;
+    std::function<void()> m_callback;
     std::string m_name;
-  };
-}
 
+    Semaphore m_semaphore;
+  };
+}  // namespace fishjoy
 
 #endif  // FISHJOY_THREAD_HPP
--- src/thread.cpp
+++ src/thread.cpp
@@ -3,75 +3,116 @@
 //
 
 #include "fishjoy/thread.hpp"
+
 #include "fishjoy/log.hpp"
 #include "fishjoy/util.hpp"
 
-namespace fishjoy {
-static thread_local Thread* t_thread = nullptr;
-static thread_local std::string t_thread_name = "UNKOWN";
+namespace fishjoy
+{
+  static fishjoy::Logger::ptr g_logger = FISHJOY_LOG_NAME("system");
 
-static fishjoy::Logger::ptr g_logger = FISHJOY_LOG_NAME("system");
-
-Thread* Thread::GetThis() {
-  return t_thread;
-}
-
-const std::string& Thread::GetName() {
-  return t_thread_name;
-}
-
-void Thread::SetName(const std::string& name) {
-  if(t_thread) {
-    t_thread->m_name = name;
-  }
-  t_thread_name = name;
-}
-
-Thread::Thread(std::function<void()> cb, const std::string& name)
-    :m_cb(cb)
-      ,m_name(name) {
-  if(name.empty()) {
-    m_name = "UNKNOW";
-  }
-  int rt = pthread_create(&m_thread, nullptr, &Thread::run, this);
-  if(rt) {
-    FISHJOY_LOG_ERROR(g_logger) << "pthread_create thread fail, rt=" << rt
-                              << " name=" << name;
-    throw std::logic_error("pthread_create error");
-  }
-}
-
-Thread::~Thread() {
-  if(m_thread) {
-    pthread_detach(m_thread);
-  }
-}
-
-void Thread::join() {
-  if(m_thread) {
-    int rt = pthread_join(m_thread, nullptr);
-    if(rt) {
-      FISHJOY_LOG_ERROR(g_logger) << "pthread_join thread fail, rt=" << rt
-                                << " name=" << m_name;
-      throw std::logic_error("pthread_join error");
+  Semaphore::Semaphore(uint32_t count)
+  {
+    if(sem_init(&m_semaphore, 0, count) != 0) {
+      throw std::logic_error("sem_init error");
     }
-    m_thread = 0;
   }
-}
 
-void* Thread::run(void* arg) {
-  Thread* thread = (Thread*)arg;
-  t_thread = thread;
-  t_thread_name = thread->m_name;
-  thread->m_id = fishjoy::GetThreadId();
-  pthread_setname_np(pthread_self(), thread->m_name.substr(0, 15).c_str());
+  Semaphore::~Semaphore()
+  {
+    sem_destroy(&m_semaphore);
+  }
 
-  std::function<void()> cb;
-  cb.swap(thread->m_cb);
+  void Semaphore::wait() {
+    if(sem_wait(&m_semaphore) != 0) {
+      throw std::logic_error("sem_wait error");
+    }
+  }
 
-  cb();
-  return 0;
-}
+  void Semaphore::notify()
+  {
+    if(sem_post(&m_semaphore) != 0) {
+      throw std::logic_error("sem_post error");
+    }
+  }
 
-}
+  static thread_local Thread* t_thread = nullptr;
+  static thread_local std::string t_thread_name = "UNKOWN";
 
+
+  Thread* Thread::GetThis()
+  {
+    return t_thread;
+  }
+
+  const std::string& Thread::GetName()
+  {
+    return t_thread_name;
+  }
+
+  void Thread::SetName(const std::string& name)
+  {
+    if (t_thread != nullptr)
+    {
+      t_thread->m_name = name;
+    }
+    t_thread_name = name;
+  }
+
+  Thread::Thread(std::function<void()> callback, const std::string& name) : m_callback(callback), m_name(name)
+  {
+    if (name.empty())
+    {
+      m_name = "UNKNOW";
+    }
+    int ret = pthread_create(&m_thread, nullptr, &Thread::run, this);
+    if (ret != 0)
+    {
+      FISHJOY_LOG_ERROR(g_logger) << "pthread_create thread fail, rt=" << ret << " name=" << name;
+      throw std::logic_error("pthread_create error");
+    }
+    m_semaphore.wait();
+  }
+
+  Thread::~Thread()
+  {
+    if (m_thread)
+    {
+      pthread_detach(m_thread);
+    }
+  }
+
+  void Thread::join()
+  {
+    if (m_thread)
+    {
+      int ret = pthread_join(m_thread, nullptr);
+      if (ret != 0)
+      {
+        FISHJOY_LOG_ERROR(g_logger) << "pthread_join thread fail, rt=" << ret << " name=" << m_name;
+        throw std::logic_error("pthread_join error");
+      }
+      m_thread = 0;
+    }
+  }
+
+  void* Thread::run(void* arg)
+  {
+    auto* thread = static_cast<Thread*>(arg);
+    t_thread = thread;
+    t_thread_name = thread->m_name;
+    thread->m_id = fishjoy::GetThreadId();
+    pthread_setname_np(pthread_self(), thread->m_name.substr(0, 15).c_str());
+
+    std::function<void()> cb;
+    cb.swap(thread->m_callback);
+
+    thread->m_semaphore.notify();
+
+    cb();
+
+    return nullptr;
+  }
+
+
+}  // namespace fishjoy
