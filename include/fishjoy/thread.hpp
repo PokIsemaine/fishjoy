108171c94ccf228662e23514be3dd7d9dcb184e0
fishingrod <851892190@qq.com>
2022-06-05 23:21:33 +0800

    tidying

--- include/fishjoy/thread.hpp
+++ include/fishjoy/thread.hpp
@@ -11,7 +11,6 @@
 #include <string>
 #include <thread>
 #include <semaphore.h>
-#include <atomic>
 #include "mutex.hpp"
 #include "noncopyable.hpp"
 
@@ -24,6 +23,20 @@ namespace fishjoy
     Thread(std::function<void()> callback, const std::string& name);
     ~Thread();
 
+    void join();
+
+   private:
+    static void* run(void* arg);
+
+
+    pid_t m_id = -1;
+    pthread_t m_thread = 0;
+    std::function<void()> m_callback;
+    std::string m_name;
+    Semaphore m_semaphore;
+
+   public:  // Getter,Setter,CRUD
+
     pid_t getId() const
     {
       return m_id;
@@ -32,27 +45,9 @@ namespace fishjoy
     {
       return m_name;
     }
-
-    void join();
-
     static Thread* GetThis();
     static const std::string& GetName();
     static void SetName(const std::string& name);
-
-    //互斥量、互斥信号不能拷贝构造
-    Thread(const Thread&) = delete;
-    Thread(const Thread&&) = delete;
-    Thread& operator=(const Thread&) = delete;
-
-   private:
-    static void* run(void* arg);
-
-    pid_t m_id = -1;
-    pthread_t m_thread = 0;
-    std::function<void()> m_callback;
-    std::string m_name;
-
-    Semaphore m_semaphore;
   };
 }  // namespace fishjoy
 
