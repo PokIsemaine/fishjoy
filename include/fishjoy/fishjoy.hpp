36f7e613dd90281c0ed6723233b3b271309c46bb
fishingrod <851892190@qq.com>
2022-06-05 23:22:54 +0800

    create a set of header files for test

---
+++ include/fishjoy/fishjoy.hpp
@@ -0,0 +1,18 @@
+//
+// Created by zsl on 6/5/22.
+//
+
+#ifndef FISHJOY_FISHJOY_HPP
+#define FISHJOY_FISHJOY_HPP
+
+#include "fishjoy/config.hpp"
+#include "fishjoy/fiber.hpp"
+#include "fishjoy/log.hpp"
+#include "fishjoy/mutex.hpp"
+#include "fishjoy/noncopyable.hpp"
+#include "fishjoy/singleton.hpp"
+#include "fishjoy/thread.hpp"
+#include "fishjoy/util.hpp"
+#include "fiber.hpp"
+
+#endif  // FISHJOY_FISHJOY_HPP
