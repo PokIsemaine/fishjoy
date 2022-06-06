ded944859447db9b0d95a950d790a1ca809bad8e
fishingrod <851892190@qq.com>
2022-06-06 17:38:58 +0800

    update config module

--- include/fishjoy/config.hpp
+++ include/fishjoy/config.hpp
@@ -1,129 +1,60 @@
-//
-// Created by zsl on 5/18/22.
-//
+#ifndef __FISHJOY_CONFIG_H__
+#define __FISHJOY_CONFIG_H__
 
-#ifndef FISHJOY_CONFIG_HPP
-#define FISHJOY_CONFIG_HPP
-
-#include <yaml-cpp/yaml.h>
-
+#include <memory>
+#include <string>
+#include <sstream>
 #include <boost/lexical_cast.hpp>
+#include <yaml-cpp/yaml.h>
+#include "fishjoy/log.hpp"
+#include <vector>
 #include <list>
 #include <map>
-#include <memory>
-#include <sstream>
-#include <string>
+#include <set>
 #include <unordered_map>
 #include <unordered_set>
+#include <functional>
 
-#include "fishjoy/log.hpp"
+namespace fishjoy {
 
-namespace fishjoy
-{
-  class ConfigVarBase
-  {
+  class ConfigVarBase {
    public:
-    using ptr = std::shared_ptr<ConfigVarBase>;
-    ConfigVarBase(const std::string& name, const std::string& description) : m_name(name), m_description(description)
-    {
+    typedef std::shared_ptr<ConfigVarBase> ptr;
+    ConfigVarBase(const std::string& name, const std::string& description = "")
+        :m_name(name)
+          ,m_description(description) {
       std::transform(m_name.begin(), m_name.end(), m_name.begin(), ::tolower);
     }
-    ~ConfigVarBase() = default;
+    virtual ~ConfigVarBase() {}
 
-    const std::string& getName() const
-    {
-      return m_name;
-    }
-    const std::string& getDescription() const
-    {
-      return m_description;
-    }
+    const std::string& getName() const { return m_name;}
+    const std::string& getDescription() const { return m_description;}
 
     virtual std::string toString() = 0;
     virtual bool fromString(const std::string& val) = 0;
-
     virtual std::string getTypeName() const = 0;
-
-   private:
+   protected:
     std::string m_name;
     std::string m_description;
   };
 
-  template<typename T>
-  class ConfigVar : public ConfigVarBase
-  {
-   public:
-    using ptr = std::shared_ptr<ConfigVar>;
-    ConfigVar(const std::string& name, const T& default_value, const std::string& description = "")
-        : ConfigVarBase(name, description),
-          m_val(default_value)
-    {
-    }
-
-    std::string toString() override
-    {
-      try
-      {
-        return boost::lexical_cast<std::string>(m_val);
-      }
-      catch (std::exception& e)
-      {
-        FISHJOY_LOG_ERROR(FISHJOY_LOG_ROOT())
-            << "ConfigVar::toString exception" << e.what() << " convert: " << typeid(m_val).name() << " to string ";
-      }
-      return "";
-    }
-
-    bool fromString(const std::string& val) override
-    {
-      try
-      {
-        m_val = boost::lexical_cast<T>(val);
-      }
-      catch (std::exception& e)
-      {
-        FISHJOY_LOG_ERROR(FISHJOY_LOG_ROOT())
-            << "ConfigVar::fromString exception" << e.what() << " convert: string to " << typeid(m_val).name();
-      }
-      return false;
-    }
-
-    const T getValue() const
-    {
-      return m_val;
-    }
-    void setValue(const T& v)
-    {
-      m_val = v;
-    }
-
-   private:
-    T m_val;
-  };
-
-  // 类型转换模板类 F from_type, T to_type
+  //F from_type, T to_type
   template<class F, class T>
-  class LexicalCast
-  {
+  class LexicalCast {
    public:
-    T operator()(const F& v)
-    {
+    T operator()(const F& v) {
       return boost::lexical_cast<T>(v);
     }
   };
 
-  // boost::lexical_cast 只能进行基本类型和std::string的转换，需要对其他类型进行特化
   template<class T>
-  class LexicalCast<std::string, std::vector<T>>
-  {
+  class LexicalCast<std::string, std::vector<T> > {
    public:
-    std::vector<T> operator()(const std::string& v)
-    {
+    std::vector<T> operator()(const std::string& v) {
       YAML::Node node = YAML::Load(v);
       typename std::vector<T> vec;
       std::stringstream ss;
-      for (size_t i = 0; i < node.size(); ++i)
-      {
+      for(size_t i = 0; i < node.size(); ++i) {
         ss.str("");
         ss << node[i];
         vec.push_back(LexicalCast<std::string, T>()(ss.str()));
@@ -133,14 +64,11 @@ namespace fishjoy
   };
 
   template<class T>
-  class LexicalCast<std::vector<T>, std::string>
-  {
+  class LexicalCast<std::vector<T>, std::string> {
    public:
-    std::string operator()(const std::vector<T>& v)
-    {
+    std::string operator()(const std::vector<T>& v) {
       YAML::Node node;
-      for (auto& i : v)
-      {
+      for(auto& i : v) {
         node.push_back(YAML::Load(LexicalCast<T, std::string>()(i)));
       }
       std::stringstream ss;
@@ -150,16 +78,13 @@ namespace fishjoy
   };
 
   template<class T>
-  class LexicalCast<std::string, std::list<T>>
-  {
+  class LexicalCast<std::string, std::list<T> > {
    public:
-    std::list<T> operator()(const std::string& v)
-    {
+    std::list<T> operator()(const std::string& v) {
       YAML::Node node = YAML::Load(v);
       typename std::list<T> vec;
       std::stringstream ss;
-      for (size_t i = 0; i < node.size(); ++i)
-      {
+      for(size_t i = 0; i < node.size(); ++i) {
         ss.str("");
         ss << node[i];
         vec.push_back(LexicalCast<std::string, T>()(ss.str()));
@@ -169,14 +94,11 @@ namespace fishjoy
   };
 
   template<class T>
-  class LexicalCast<std::list<T>, std::string>
-  {
+  class LexicalCast<std::list<T>, std::string> {
    public:
-    std::string operator()(const std::list<T>& v)
-    {
+    std::string operator()(const std::list<T>& v) {
       YAML::Node node;
-      for (auto& i : v)
-      {
+      for(auto& i : v) {
         node.push_back(YAML::Load(LexicalCast<T, std::string>()(i)));
       }
       std::stringstream ss;
@@ -186,16 +108,13 @@ namespace fishjoy
   };
 
   template<class T>
-  class LexicalCast<std::string, std::set<T>>
-  {
+  class LexicalCast<std::string, std::set<T> > {
    public:
-    std::set<T> operator()(const std::string& v)
-    {
+    std::set<T> operator()(const std::string& v) {
       YAML::Node node = YAML::Load(v);
       typename std::set<T> vec;
       std::stringstream ss;
-      for (size_t i = 0; i < node.size(); ++i)
-      {
+      for(size_t i = 0; i < node.size(); ++i) {
         ss.str("");
         ss << node[i];
         vec.insert(LexicalCast<std::string, T>()(ss.str()));
@@ -205,14 +124,11 @@ namespace fishjoy
   };
 
   template<class T>
-  class LexicalCast<std::set<T>, std::string>
-  {
+  class LexicalCast<std::set<T>, std::string> {
    public:
-    std::string operator()(const std::set<T>& v)
-    {
+    std::string operator()(const std::set<T>& v) {
       YAML::Node node;
-      for (auto& i : v)
-      {
+      for(auto& i : v) {
         node.push_back(YAML::Load(LexicalCast<T, std::string>()(i)));
       }
       std::stringstream ss;
@@ -222,16 +138,13 @@ namespace fishjoy
   };
 
   template<class T>
-  class LexicalCast<std::string, std::unordered_set<T>>
-  {
+  class LexicalCast<std::string, std::unordered_set<T> > {
    public:
-    std::unordered_set<T> operator()(const std::string& v)
-    {
+    std::unordered_set<T> operator()(const std::string& v) {
       YAML::Node node = YAML::Load(v);
       typename std::unordered_set<T> vec;
       std::stringstream ss;
-      for (size_t i = 0; i < node.size(); ++i)
-      {
+      for(size_t i = 0; i < node.size(); ++i) {
         ss.str("");
         ss << node[i];
         vec.insert(LexicalCast<std::string, T>()(ss.str()));
@@ -241,14 +154,11 @@ namespace fishjoy
   };
 
   template<class T>
-  class LexicalCast<std::unordered_set<T>, std::string>
-  {
+  class LexicalCast<std::unordered_set<T>, std::string> {
    public:
-    std::string operator()(const std::unordered_set<T>& v)
-    {
+    std::string operator()(const std::unordered_set<T>& v) {
       YAML::Node node;
-      for (auto& i : v)
-      {
+      for(auto& i : v) {
         node.push_back(YAML::Load(LexicalCast<T, std::string>()(i)));
       }
       std::stringstream ss;
@@ -257,50 +167,196 @@ namespace fishjoy
     }
   };
 
-  class Config
-  {
+  template<class T>
+  class LexicalCast<std::string, std::map<std::string, T> > {
    public:
-    using ConfigVarMap = std::map<std::string, ConfigVarBase::ptr>;
+    std::map<std::string, T> operator()(const std::string& v) {
+      YAML::Node node = YAML::Load(v);
+      typename std::map<std::string, T> vec;
+      std::stringstream ss;
+      for(auto it = node.begin();
+           it != node.end(); ++it) {
+        ss.str("");
+        ss << it->second;
+        vec.insert(std::make_pair(it->first.Scalar(),
+                                  LexicalCast<std::string, T>()(ss.str())));
+      }
+      return vec;
+    }
+  };
 
-    template<typename T>
-    static typename ConfigVar<T>::ptr
-    Lookup(const std::string& name, const T& default_value, const std::string& description = "")
-    {
-      auto tmp = Lookup<T>(name);
-      if (tmp)
-      {
-        FISHJOY_LOG_INFO(FISHJOY_LOG_ROOT()) << "Lookup name=" << name << " exists";
-        return tmp;
+  template<class T>
+  class LexicalCast<std::map<std::string, T>, std::string> {
+   public:
+    std::string operator()(const std::map<std::string, T>& v) {
+      YAML::Node node;
+      for(auto& i : v) {
+        node[i.first] = YAML::Load(LexicalCast<T, std::string>()(i.second));
+      }
+      std::stringstream ss;
+      ss << node;
+      return ss.str();
+    }
+  };
+
+  template<class T>
+  class LexicalCast<std::string, std::unordered_map<std::string, T> > {
+   public:
+    std::unordered_map<std::string, T> operator()(const std::string& v) {
+      YAML::Node node = YAML::Load(v);
+      typename std::unordered_map<std::string, T> vec;
+      std::stringstream ss;
+      for(auto it = node.begin();
+           it != node.end(); ++it) {
+        ss.str("");
+        ss << it->second;
+        vec.insert(std::make_pair(it->first.Scalar(),
+                                  LexicalCast<std::string, T>()(ss.str())));
+      }
+      return vec;
+    }
+  };
+
+  template<class T>
+  class LexicalCast<std::unordered_map<std::string, T>, std::string> {
+   public:
+    std::string operator()(const std::unordered_map<std::string, T>& v) {
+      YAML::Node node;
+      for(auto& i : v) {
+        node[i.first] = YAML::Load(LexicalCast<T, std::string>()(i.second));
+      }
+      std::stringstream ss;
+      ss << node;
+      return ss.str();
+    }
+  };
+
+
+  //FromStr T operator()(const std::string&) 
+  //ToStr std::string operator()(const T&)
+  template<class T, class FromStr = LexicalCast<std::string, T>
+           ,class ToStr = LexicalCast<T, std::string> >
+  class ConfigVar : public ConfigVarBase {
+   public:
+    typedef std::shared_ptr<ConfigVar> ptr;
+    typedef std::function<void (const T& old_value, const T& new_value)> on_change_cb;
+
+    ConfigVar(const std::string& name
+              ,const T& default_value
+              ,const std::string& description = "")
+        :ConfigVarBase(name, description)
+          ,m_val(default_value) {
+    }
+
+    std::string toString() override {
+      try {
+        //return boost::lexical_cast<std::string>(m_val);
+        return ToStr()(m_val);
+      } catch (std::exception& e) {
+        FISHJOY_LOG_ERROR(FISHJOY_LOG_ROOT()) << "ConfigVar::toString exception"
+                                          << e.what() << " convert: " << typeid(m_val).name() << " to string";
+      }
+      return "";
+    }
+
+    bool fromString(const std::string& val) override {
+      try {
+        //m_val = boost::lexical_cast<T>(val);
+        setValue(FromStr()(val));
+      } catch (std::exception& e) {
+        FISHJOY_LOG_ERROR(FISHJOY_LOG_ROOT()) << "ConfigVar::toString exception"
+                                          << e.what() << " convert: string to " << typeid(m_val).name()
+                                          << " - " << val;
+      }
+      return false;
+    }
+
+    const T getValue() const { return m_val;}
+
+    void setValue(const T& v) {
+      if(v == m_val) {
+        return;
+      }
+      for(auto& i : m_cbs) {
+        i.second(m_val, v);
+      }
+      m_val = v;
+    }
+
+    std::string getTypeName() const override { return typeid(T).name();}
+
+    void addListener(uint64_t key, on_change_cb cb) {
+      m_cbs[key] = cb;
+    }
+
+    void delListener(uint64_t key) {
+      m_cbs.erase(key);
+    }
+
+    on_change_cb getListener(uint64_t key) {
+      auto it = m_cbs.find(key);
+      return it == m_cbs.end() ? nullptr : it->second;
+    }
+
+    void clearListener() {
+      m_cbs.clear();
+    }
+   private:
+    T m_val;
+    //变更回调函数组, uint64_t key,要求唯一，一般可以用hash
+    std::map<uint64_t, on_change_cb> m_cbs;
+  };
+
+  class Config {
+   public:
+    typedef std::unordered_map<std::string, ConfigVarBase::ptr> ConfigVarMap;
+
+    template<class T>
+    static typename ConfigVar<T>::ptr Lookup(const std::string& name,
+                                             const T& default_value, const std::string& description = "") {
+      auto it = GetDatas().find(name);
+      if(it != GetDatas().end()) {
+        auto tmp = std::dynamic_pointer_cast<ConfigVar<T> >(it->second);
+        if(tmp) {
+          FISHJOY_LOG_INFO(FISHJOY_LOG_ROOT()) << "Lookup name=" << name << " exists";
+          return tmp;
+        } else {
+          FISHJOY_LOG_ERROR(FISHJOY_LOG_ROOT()) << "Lookup name=" << name << " exists but type not "
+                                            << typeid(T).name() << " real_type=" << it->second->getTypeName()
+                                            << " " << it->second->toString();
+          return nullptr;
+        }
       }
 
-      if (name.find_first_not_of("abcdefghijklmnopqrstuvwxyz._012345678") != std::string::npos)
-      {
-        FISHJOY_LOG_ERROR(FISHJOY_LOG_ROOT()) << "Lookup name invalid" << name;
+      if(name.find_first_not_of("abcdefghikjlmnopqrstuvwxyz._012345678")
+          != std::string::npos) {
+        FISHJOY_LOG_ERROR(FISHJOY_LOG_ROOT()) << "Lookup name invalid " << name;
         throw std::invalid_argument(name);
       }
 
       typename ConfigVar<T>::ptr v(new ConfigVar<T>(name, default_value, description));
-      s_datas[name] = v;
+      GetDatas()[name] = v;
       return v;
     }
 
-    template<typename T>
-    static typename ConfigVar<T>::ptr Lookup(const std::string& name)
-    {
-      auto it = s_datas.find(name);
-      if (it == s_datas.end())
-      {
+    template<class T>
+    static typename ConfigVar<T>::ptr Lookup(const std::string& name) {
+      auto it = GetDatas().find(name);
+      if(it == GetDatas().end()) {
         return nullptr;
       }
-      return std::dynamic_pointer_cast<ConfigVar<T>>(it->second);
+      return std::dynamic_pointer_cast<ConfigVar<T> >(it->second);
     }
 
     static void LoadFromYaml(const YAML::Node& root);
     static ConfigVarBase::ptr LookupBase(const std::string& name);
-
    private:
-    static ConfigVarMap s_datas;
+    static ConfigVarMap& GetDatas() {
+      static ConfigVarMap s_datas;
+      return s_datas;
+    }
   };
-}  // namespace fishjoy
 
-#endif  // FISHJOY_CONFIG_HPP
+}
+
+#endif
--- src/config.cpp
+++ src/config.cpp
@@ -5,58 +5,51 @@
 
 namespace fishjoy
 {
-  Config::ConfigVarMap Config::s_datas;
-
-  ConfigVarBase::ptr Config::LookupBase(const std::string& name)
-  {
-    auto it = s_datas.find(name);
-    return it == s_datas.end() ? nullptr : it->second;
+  ConfigVarBase::ptr Config::LookupBase(const std::string& name) {
+    auto it = GetDatas().find(name);
+    return it == GetDatas().end() ? nullptr : it->second;
   }
 
-  static void ListAllMember(
-      const std::string& prefix,
-      const YAML::Node& node,
-      std::list<std::pair<std::string, const YAML::Node>>& output)
-  {
-    if (prefix.find_first_not_of("abcdefghikjlmnopqrstuvwxyz._012345678") != std::string::npos)
-    {
+  //"A.B", 10
+  //A:
+  //  B: 10
+  //  C: str
+
+  static void ListAllMember(const std::string& prefix,
+                            const YAML::Node& node,
+                            std::list<std::pair<std::string, const YAML::Node> >& output) {
+    if(prefix.find_first_not_of("abcdefghikjlmnopqrstuvwxyz._012345678")
+        != std::string::npos) {
       FISHJOY_LOG_ERROR(FISHJOY_LOG_ROOT()) << "Config invalid name: " << prefix << " : " << node;
       return;
     }
-    output.emplace_back(prefix, node);
-    if (node.IsMap())
-    {
-      for (auto it = node.begin(); it != node.end(); ++it)
-      {
-        ListAllMember(prefix.empty() ? it->first.Scalar() : prefix + "." + it->first.Scalar(), it->second, output);
+    output.push_back(std::make_pair(prefix, node));
+    if(node.IsMap()) {
+      for(auto it = node.begin();
+           it != node.end(); ++it) {
+        ListAllMember(prefix.empty() ? it->first.Scalar()
+                                     : prefix + "." + it->first.Scalar(), it->second, output);
       }
     }
   }
 
-  void Config::LoadFromYaml(const YAML::Node& root)
-  {
-    std::list<std::pair<std::string, const YAML::Node>> all_nodes;
+  void Config::LoadFromYaml(const YAML::Node& root) {
+    std::list<std::pair<std::string, const YAML::Node> > all_nodes;
     ListAllMember("", root, all_nodes);
 
-    for (auto& i : all_nodes)
-    {
+    for(auto& i : all_nodes) {
       std::string key = i.first;
-      if (key.empty())
-      {
+      if(key.empty()) {
         continue;
       }
 
       std::transform(key.begin(), key.end(), key.begin(), ::tolower);
       ConfigVarBase::ptr var = LookupBase(key);
 
-      if (var)
-      {
-        if (i.second.IsScalar())
-        {
+      if(var) {
+        if(i.second.IsScalar()) {
           var->fromString(i.second.Scalar());
-        }
-        else
-        {
+        } else {
           std::stringstream ss;
           ss << i.second;
           var->fromString(ss.str());
