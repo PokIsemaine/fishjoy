0fe96c83abaad9d423c70e5e12c9bfe6cfd9fd52
fishingrod <851892190@qq.com>
2022-06-26 14:07:17 +0800

    build(SourcesAndHeaders.cmake): add env

--- cmake/SourcesAndHeaders.cmake
+++ cmake/SourcesAndHeaders.cmake
@@ -6,6 +6,7 @@ set(sources
 	src/mutex.cpp
 	src/fiber.cpp
 	src/scheduler.cpp
+	src/env.cpp
 )
 
 set(exe_sources
@@ -22,14 +23,14 @@ set(headers
 	include/fishjoy/mutex.hpp
 	include/fishjoy/fiber.hpp
 	include/fishjoy/scheduler.hpp
+	include/fishjoy/env.hpp
 )
 
 set(test_sources
-#  	src/log_test.cpp
-#  	src/log_units_test.cpp
+  	src/test_log.cpp
 #  	src/test_thread.cpp
-#	src/test_config.cpp
+	src/test_config.cpp
 #	src/test_fiber.cpp
 #	src/test_fiber2.cpp
-  	src/test_scheduler.cpp
+#  	src/test_scheduler.cpp
 )
