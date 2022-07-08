9b1b6cb807de2d4b1f18d1e20149e12707f04f44
fishingrod <851892190@qq.com>
2022-07-08 10:49:44 +0800

    feat(endian.hpp): 判断大小端+字节序转换

---
+++ include/fishjoy/endian.hpp
@@ -0,0 +1,89 @@
+//
+// Created by zsl on 7/8/22.
+//
+
+#ifndef FISHJOY_ENDIAN_HPP
+#define FISHJOY_ENDIAN_HPP
+
+#include <byteswap.h>
+#include <stdint.h>
+#include <type_traits>
+
+#define FISHJOY_LITTLE_ENDIAN 1
+#define FISHJOY_BIG_ENDIAN 2
+
+namespace fishjoy {
+
+  /**
+ * @brief 8字节类型的字节序转化
+   */
+  template <class T>
+  typename std::enable_if<sizeof(T) == sizeof(uint64_t), T>::type
+  byteswap(T value) {
+    return (T)bswap_64((uint64_t)value);
+  }
+
+  /**
+ * @brief 4字节类型的字节序转化
+   */
+  template <class T>
+  typename std::enable_if<sizeof(T) == sizeof(uint32_t), T>::type
+  byteswap(T value) {
+    return (T)bswap_32((uint32_t)value);
+  }
+
+  /**
+ * @brief 2字节类型的字节序转化
+   */
+  template <class T>
+  typename std::enable_if<sizeof(T) == sizeof(uint16_t), T>::type
+  byteswap(T value) {
+    return (T)bswap_16((uint16_t)value);
+  }
+
+  // endian.h
+#if BYTE_ORDER == BIG_ENDIAN
+#define FISHJOY_BYTE_ORDER FISHJOY_BIG_ENDIAN
+#else
+#define FISHJOY_BYTE_ORDER FISHJOY_LITTLE_ENDIAN
+#endif
+
+#if FISHJOY_BYTE_ORDER == FISHJOY_BIG_ENDIAN
+
+  /**
+ * @brief 只在小端机器上执行byteswap, 在大端机器上什么都不做
+   */
+  template <class T>
+  T byteswapOnLittleEndian(T t) {
+    return t;
+  }
+
+  /**
+ * @brief 只在大端机器上执行byteswap, 在小端机器上什么都不做
+   */
+  template <class T>
+  T byteswapOnBigEndian(T t) {
+    return byteswap(t);
+  }
+#else
+
+  /**
+ * @brief 只在小端机器上执行byteswap, 在大端机器上什么都不做
+   */
+  template <class T>
+  T byteswapOnLittleEndian(T t) {
+    return byteswap(t);
+  }
+
+  /**
+ * @brief 只在大端机器上执行byteswap, 在小端机器上什么都不做
+   */
+  template <class T>
+  T byteswapOnBigEndian(T t) {
+    return t;
+  }
+#endif
+
+}
+
+#endif  // FISHJOY_ENDIAN_HPP
