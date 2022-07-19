5400dc432540e922ac153dff9aa947c43be4f541
fishingrod <851892190@qq.com>
2022-07-19 16:35:41 +0800

    feat(util.hpp util.cpp): 添加 字符串辅助 类 和一些辅助工具函数

--- include/fishjoy/util.hpp
+++ include/fishjoy/util.hpp
@@ -21,19 +21,81 @@
 
 namespace fishjoy
 {
+  /**
+ * @brief 获取线程id
+ * @note 这里不要把pid_t和pthread_t混淆，关于它们之的区别可参考gettid(2)
+   */
   pid_t GetThreadId();
-  uint32_t GetFiberId();
 
-  void Backtrace(std::vector<std::string>& bt, unsigned long size, int skip = 1);
-  std::string BacktraceToString(unsigned long size, int skip = 2, const std::string& prefix = "");
-
+  /**
+ * @brief 获取协程id
+ * @todo 桩函数，暂时返回0，等协程模块完善后再返回实际值
+   */
+  uint64_t GetFiberId();
 
   /**
  * @brief 获取当前启动的毫秒数，参考clock_gettime(2)，使用CLOCK_MONOTONIC_RAW
    */
   uint64_t GetElapsedMS();
 
+  /**
+ * @brief 获取线程名称，参考pthread_getname_np(3)
+   */
+  std::string GetThreadName();
 
+  /**
+ * @brief 设置线程名称，参考pthread_setname_np(3)
+ * @note 线程名称不能超过16字节，包括结尾的'\0'字符
+   */
+  void SetThreadName(const std::string &name);
+
+  /**
+ * @brief 获取当前的调用栈
+ * @param[out] bt 保存调用栈
+ * @param[in] size 最多返回层数
+ * @param[in] skip 跳过栈顶的层数
+   */
+  void Backtrace(std::vector<std::string> &bt, int size = 64, int skip = 1);
+
+  /**
+ * @brief 获取当前栈信息的字符串
+ * @param[in] size 栈的最大层数
+ * @param[in] skip 跳过栈顶的层数
+ * @param[in] prefix 栈信息前输出的内容
+   */
+  std::string BacktraceToString(int size = 64, int skip = 2, const std::string &prefix = "");
+
+  /**
+ * @brief 获取当前时间的毫秒
+   */
+  uint64_t GetCurrentMS();
+
+  /**
+ * @brief 获取当前时间的微秒
+   */
+  uint64_t GetCurrentUS();
+
+  /**
+ * @brief 字符串转大写
+   */
+  std::string ToUpper(const std::string &name);
+
+  /**
+ * @brief 字符串转小写
+   */
+  std::string ToLower(const std::string &name);
+
+  /**
+ * @brief 日期时间转字符串
+   */
+  std::string Time2Str(time_t ts = time(0), const std::string &format = "%Y-%m-%d %H:%M:%S");
+
+  /**
+ * @brief 字符串转日期时间
+   */
+  time_t Str2Time(const char *str, const char *format = "%Y-%m-%d %H:%M:%S");
+
+  //TODO C++17 filesystem
   /**
  * @brief 文件系统操作类
    */
@@ -133,6 +195,73 @@ namespace fishjoy
      * @return  是否打开成功
      */
     static bool OpenForWrite(std::ofstream &ofs, const std::string &filename, std::ios_base::openmode mode);
+  };
+
+  /**
+ * @brief 字符串辅助类
+   */
+  class StringUtil {
+   public:
+    /**
+     * @brief printf风格的字符串格式化，返回格式化后的string
+     */
+    static std::string Format(const char* fmt, ...);
+
+    /**
+     * @brief vprintf风格的字符串格式化，返回格式化后的string
+     */
+    static std::string Formatv(const char* fmt, va_list ap);
+
+    /**
+     * @brief url编码
+     * @param[in] str 原始字符串
+     * @param[in] space_as_plus 是否将空格编码成+号，如果为false，则空格编码成%20
+     * @return 编码后的字符串
+     */
+    static std::string UrlEncode(const std::string& str, bool space_as_plus = true);
+
+    /**
+     * @brief url解码
+     * @param[in] str url字符串
+     * @param[in] space_as_plus 是否将+号解码为空格
+     * @return 解析后的字符串
+     */
+    static std::string UrlDecode(const std::string& str, bool space_as_plus = true);
+
+    /**
+     * @brief 移除字符串首尾的指定字符串
+     * @param[] str 输入字符串
+     * @param[] delimit 待移除的字符串
+     * @return  移除后的字符串
+     */
+    static std::string Trim(const std::string& str, const std::string& delimit = " \t\r\n");
+
+    /**
+     * @brief 移除字符串首部的指定字符串
+     * @param[] str 输入字符串
+     * @param[] delimit 待移除的字符串
+     * @return  移除后的字符串
+     */
+    static std::string TrimLeft(const std::string& str, const std::string& delimit = " \t\r\n");
+
+    /**
+     * @brief 移除字符尾部的指定字符串
+     * @param[] str 输入字符串
+     * @param[] delimit 待移除的字符串
+     * @return  移除后的字符串
+     */
+    static std::string TrimRight(const std::string& str, const std::string& delimit = " \t\r\n");
+
+    /**
+     * @brief 宽字符串转字符串
+     */
+    static std::string WStringToString(const std::wstring& ws);
+
+    /**
+     * @brief 字符串转宽字符串
+     */
+    static std::wstring StringToWString(const std::string& s);
+
   };
 
 }  // namespace fishjoy
--- src/util.cpp
+++ src/util.cpp
@@ -3,54 +3,127 @@
 //
 
 #include "fishjoy/util.hpp"
-
-#include <execinfo.h>
 #include <string.h>
+#include <execinfo.h> // for backtrace()
+#include <cxxabi.h>   // for abi::__cxa_demangle()
+#include <algorithm>  // for std::transform()
 
 #include "fishjoy/log.hpp"
 #include "fishjoy/fiber.hpp"
 
 namespace fishjoy {
-  fishjoy::Logger::ptr g_logger = FISHJOY_LOG_NAME("system");
+  static fishjoy::Logger::ptr g_logger = FISHJOY_LOG_NAME("system");
+  
+  pid_t GetThreadId() {
+    return syscall(SYS_gettid);
+  }
 
-  pid_t GetThreadId() { return static_cast<pid_t>(syscall(SYS_gettid)); }
+  uint64_t GetFiberId() {
+    return Fiber::GetFiberId();
+  }
 
-  uint32_t GetFiberId() { return Fiber::GetFiberId(); }
+  uint64_t GetElapsedMS() {
+    struct timespec ts = {0};
+    clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
+    return ts.tv_sec * 1000 + ts.tv_nsec / 1000000;
+  }
 
-  void Backtrace(std::vector<std::string>& bt, size_t size, int skip) {
-    void** array = (void**)malloc(sizeof(void*) * size);
-    size_t s = static_cast<size_t>(::backtrace(array, size));
+  std::string GetThreadName() {
+    char thread_name[16] = {0};
+    pthread_getname_np(pthread_self(), thread_name, 16);
+    return std::string(thread_name);
+  }
 
-    char** strings = backtrace_symbols(array, s);
+  void SetThreadName(const std::string &name) {
+    pthread_setname_np(pthread_self(), name.substr(0, 15).c_str());
+  }
+
+  static std::string demangle(const char *str) {
+    size_t size = 0;
+    int status  = 0;
+    std::string rt;
+    rt.resize(256);
+    if (1 == sscanf(str, "%*[^(]%*[^_]%255[^)+]", &rt[0])) {
+      char *v = abi::__cxa_demangle(&rt[0], nullptr, &size, &status);
+      if (v) {
+        std::string result(v);
+        free(v);
+        return result;
+      }
+    }
+    if (1 == sscanf(str, "%255s", &rt[0])) {
+      return rt;
+    }
+    return str;
+  }
+
+  void Backtrace(std::vector<std::string> &bt, int size, int skip) {
+    void **array = (void **)malloc((sizeof(void *) * size));
+    size_t s     = ::backtrace(array, size);
+
+    char **strings = backtrace_symbols(array, s);
     if (strings == NULL) {
-      FISHJOY_LOG_ERROR(g_logger) << "backtrace_symbols error";
+      FISHJOY_LOG_ERROR(g_logger) << "backtrace_synbols error";
       return;
     }
 
     for (size_t i = skip; i < s; ++i) {
-      bt.push_back(strings[i]);
+      bt.push_back(demangle(strings[i]));
     }
 
     free(strings);
     free(array);
   }
 
-  std::string BacktraceToString(unsigned long size, int skip, const std::string& prefix) {
+  std::string BacktraceToString(int size, int skip, const std::string &prefix) {
     std::vector<std::string> bt;
     Backtrace(bt, size, skip);
     std::stringstream ss;
     for (size_t i = 0; i < bt.size(); ++i) {
       ss << prefix << bt[i] << std::endl;
     }
-
     return ss.str();
   }
 
+  uint64_t GetCurrentMS() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec * 1000ul + tv.tv_usec / 1000;
+  }
 
-  uint64_t GetElapsedMS() {
-    struct timespec ts = {0};
-    clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
-    return ts.tv_sec * 1000 + ts.tv_nsec / 1000000;
+  uint64_t GetCurrentUS() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec * 1000 * 1000ul + tv.tv_usec;
+  }
+
+  std::string ToUpper(const std::string &name) {
+    std::string rt = name;
+    std::transform(rt.begin(), rt.end(), rt.begin(), ::toupper);
+    return rt;
+  }
+
+  std::string ToLower(const std::string &name) {
+    std::string rt = name;
+    std::transform(rt.begin(), rt.end(), rt.begin(), ::tolower);
+    return rt;
+  }
+
+  std::string Time2Str(time_t ts, const std::string &format) {
+    struct tm tm;
+    localtime_r(&ts, &tm);
+    char buf[64];
+    strftime(buf, sizeof(buf), format.c_str(), &tm);
+    return buf;
+  }
+
+  time_t Str2Time(const char *str, const char *format) {
+    struct tm t;
+    memset(&t, 0, sizeof(t));
+    if (!strptime(str, format, &t)) {
+      return 0;
+    }
+    return mktime(&t);
   }
 
   void FSUtil::ListAllFile(std::vector<std::string> &files, const std::string &path, const std::string &subfix) {
@@ -253,5 +326,164 @@ namespace fishjoy {
     return ofs.is_open();
   }
 
+
+  static const char uri_chars[256] = {
+    /* 0 */
+    0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 1, 1, 0,
+    1, 1, 1, 1, 1, 1, 1, 1,   1, 1, 0, 0, 0, 1, 0, 0,
+    /* 64 */
+    0, 1, 1, 1, 1, 1, 1, 1,   1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1,   1, 1, 1, 0, 0, 0, 0, 1,
+    0, 1, 1, 1, 1, 1, 1, 1,   1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1,   1, 1, 1, 0, 0, 0, 1, 0,
+    /* 128 */
+    0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,
+    /* 192 */
+    0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,
+  };
+
+  static const char xdigit_chars[256] = {
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,1,2,3,4,5,6,7,8,9,0,0,0,0,0,0,
+    0,10,11,12,13,14,15,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,10,11,12,13,14,15,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+  };
+
+#define CHAR_IS_UNRESERVED(c)           \
+    (uri_chars[(unsigned char)(c)])
+
+  //-.0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz~
+  std::string StringUtil::UrlEncode(const std::string& str, bool space_as_plus) {
+    static const char *hexdigits = "0123456789ABCDEF";
+    std::string* ss = nullptr;
+    const char* end = str.c_str() + str.length();
+    for(const char* c = str.c_str() ; c < end; ++c) {
+      if(!CHAR_IS_UNRESERVED(*c)) {
+        if(!ss) {
+          ss = new std::string;
+          ss->reserve(str.size() * 1.2);
+          ss->append(str.c_str(), c - str.c_str());
+        }
+        if(*c == ' ' && space_as_plus) {
+          ss->append(1, '+');
+        } else {
+          ss->append(1, '%');
+          ss->append(1, hexdigits[(uint8_t)*c >> 4]);
+          ss->append(1, hexdigits[*c & 0xf]);
+        }
+      } else if(ss) {
+        ss->append(1, *c);
+      }
+    }
+    if(!ss) {
+      return str;
+    } else {
+      std::string rt = *ss;
+      delete ss;
+      return rt;
+    }
+  }
+
+  std::string StringUtil::UrlDecode(const std::string& str, bool space_as_plus) {
+    std::string* ss = nullptr;
+    const char* end = str.c_str() + str.length();
+    for(const char* c = str.c_str(); c < end; ++c) {
+      if(*c == '+' && space_as_plus) {
+        if(!ss) {
+          ss = new std::string;
+          ss->append(str.c_str(), c - str.c_str());
+        }
+        ss->append(1, ' ');
+      } else if(*c == '%' && (c + 2) < end
+                 && isxdigit(*(c + 1)) && isxdigit(*(c + 2))){
+        if(!ss) {
+          ss = new std::string;
+          ss->append(str.c_str(), c - str.c_str());
+        }
+        ss->append(1, (char)(xdigit_chars[(int)*(c + 1)] << 4 | xdigit_chars[(int)*(c + 2)]));
+        c += 2;
+      } else if(ss) {
+        ss->append(1, *c);
+      }
+    }
+    if(!ss) {
+      return str;
+    } else {
+      std::string rt = *ss;
+      delete ss;
+      return rt;
+    }
+  }
+
+  std::string StringUtil::Trim(const std::string& str, const std::string& delimit) {
+    auto begin = str.find_first_not_of(delimit);
+    if(begin == std::string::npos) {
+      return "";
+    }
+    auto end = str.find_last_not_of(delimit);
+    return str.substr(begin, end - begin + 1);
+  }
+
+  std::string StringUtil::TrimLeft(const std::string& str, const std::string& delimit) {
+    auto begin = str.find_first_not_of(delimit);
+    if(begin == std::string::npos) {
+      return "";
+    }
+    return str.substr(begin);
+  }
+
+  std::string StringUtil::TrimRight(const std::string& str, const std::string& delimit) {
+    auto end = str.find_last_not_of(delimit);
+    if(end == std::string::npos) {
+      return "";
+    }
+    return str.substr(0, end);
+  }
+
+  std::string StringUtil::WStringToString(const std::wstring& ws) {
+    std::string str_locale = setlocale(LC_ALL, "");
+    const wchar_t* wch_src = ws.c_str();
+    size_t n_dest_size = wcstombs(NULL, wch_src, 0) + 1;
+    char *ch_dest = new char[n_dest_size];
+    memset(ch_dest,0,n_dest_size);
+    wcstombs(ch_dest,wch_src,n_dest_size);
+    std::string str_result = ch_dest;
+    delete []ch_dest;
+    setlocale(LC_ALL, str_locale.c_str());
+    return str_result;
+  }
+
+  std::wstring StringUtil::StringToWString(const std::string& s) {
+    std::string str_locale = setlocale(LC_ALL, "");
+    const char* chSrc = s.c_str();
+    size_t n_dest_size = mbstowcs(NULL, chSrc, 0) + 1;
+    wchar_t* wch_dest = new wchar_t[n_dest_size];
+    wmemset(wch_dest, 0, n_dest_size);
+    mbstowcs(wch_dest,chSrc,n_dest_size);
+    std::wstring wstr_result = wch_dest;
+    delete []wch_dest;
+    setlocale(LC_ALL, str_locale.c_str());
+    return wstr_result;
+  }
 
 }  // namespace fishjoy
