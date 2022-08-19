58ff3ebfb25549ab8a588c788f7cab3171204138
fishingrod <851892190@qq.com>
2022-08-19 18:39:20 +0800

    refactor(http_connection.cpp): 直接复用了 Node.js http 删除不不要的代码

--- src/http/http_connection.cpp
+++ src/http/http_connection.cpp
@@ -58,78 +58,7 @@ namespace fishjoy {
           break;
         }
       } while(true);
-      // auto& client_parser = parser->getParser();
-      // if(client_parser.chunked) {
-      //     std::string body;
-      //     int len = offset;
-      //     do {
-      //         do {
-      //             int rt = read(data + len, buff_size - len);
-      //             if(rt <= 0) {
-      //                 close();
-      //                 return nullptr;
-      //             }
-      //             len += rt;
-      //             data[len] = '\0';
-      //             size_t nparse = parser->execute(data, len, true);
-      //             if(parser->hasError()) {
-      //                 close();
-      //                 return nullptr;
-      //             }
-      //             len -= nparse;
-      //             if(len == (int)buff_size) {
-      //                 close();
-      //                 return nullptr;
-      //             }
-      //         } while(!parser->isFinished());
-      //         len -= 2;
-            
-      //         FISHJOY_LOG_INFO(g_logger) << "content_len=" << client_parser.content_len;
-      //         if(client_parser.content_len <= len) {
-      //             body.append(data, client_parser.content_len);
-      //             memmove(data, data + client_parser.content_len
-      //                     , len - client_parser.content_len);
-      //             len -= client_parser.content_len;
-      //         } else {
-      //             body.append(data, len);
-      //             int left = client_parser.content_len - len;
-      //             while(left > 0) {
-      //                 int rt = read(data, left > (int)buff_size ? (int)buff_size : left);
-      //                 if(rt <= 0) {
-      //                     close();
-      //                     return nullptr;
-      //                 }
-      //                 body.append(data, rt);
-      //                 left -= rt;
-      //             }
-      //             len = 0;
-      //         }
-      //     } while(!client_parser.chunks_done);
-      //     parser->getData()->setBody(body);
-      // } else {
-      //     int64_t length = parser->getContentLength();
-      //     if(length > 0) {
-      //         std::string body;
-      //         body.resize(length);
 
-      //         int len = 0;
-      //         if(length >= offset) {
-      //             memcpy(&body[0], data, offset);
-      //             len = offset;
-      //         } else {
-      //             memcpy(&body[0], data, length);
-      //             len = length;
-      //         }
-      //         length -= offset;
-      //         if(length > 0) {
-      //             if(readFixSize(&body[len], length) <= 0) {
-      //                 close();
-      //                 return nullptr;
-      //             }
-      //         }
-      //         parser->getData()->setBody(body);
-      //     }
-      // }
       return parser->getData();
     }
 
